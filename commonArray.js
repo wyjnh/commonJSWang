aa839d358b0c8bd0d3dd0d8369fab012012db5c1
wangyujue <wangyujue@sogou-inc.com>
2020-09-04 01:25:44 +0800

    fix: update

--- commonArray.js
+++ commonArray.js
@@ -16,6 +16,91 @@ let CommonArray = {
         })
     },
     /**
+     * 两个数组并集
+     * @param {*} arr1 
+     * @param {*} arr2 
+     */
+    getArrAll(arr1,arr2){
+        return Array.from(new Set(arr1.concat(arr2)));
+        // return arr1.concat(arr2.filter(item=>!arr1.includes(item)))
+    },
+    /**
+     * 数组1相对于2的 差集
+     * @param {*} arr1 
+     * @param {*} arr2 
+     */
+    getArrReduce(arr1,arr2){
+        return arr1.filter(item=>!new Set(arr2).has(item))
+    },
+    /**
+     * 获取两个数组的补集 各自没有的
+     * @param {*} arr1 
+     * @param {*} arr2 
+     */
+    getArrOther(arr1,arr2){
+        return Array.from(new Set(arr1.concat(arr2))).filter(item=>{
+            return !new Set(arr1).has(item) || !new Set(arr2).has(item)
+        })
+    },
+    /**
+     * 数组去重
+     * @param {*} arr 
+     */
+    arrDeduplication(arr){
+        return Array.from(new Set(arr));
+        // return [...new Set(arr)]
+    },
+    /**
+     * 数组排序
+     * @param {*} arr 
+     * @param {*} type  升序降序
+     * @param {*} key 是否是数组对象
+     */
+    arrSort(arr,type='dec',key=''){
+        if(type === 'dec'){
+            if(!!key){
+                return arr.sort((a,b)=>a[key] - b[key])
+            }else{
+                return arr.sort((a,b)=> a-b);
+            }
+        }else if(type === 'rev'){
+            if(!!key){
+                return arr.sort((a,b)=>b[key] - a[key])
+            }else{
+                return arr.sort((a,b)=> b-a);
+            }
+        }
+    },
+    /**
+     * 获取数组中最大值
+     * @param {*} arr 
+     */
+    arrGetMax(arr){
+        return arr.reduce((pre,cur,curindex,arr)=>{
+            return Math.max(pre,cur)
+        })
+    },
+    /**
+     * 数组求和
+     * @param {*} arr 
+     */
+    arrGetSum(arr){
+        return arr.reduce((prev,cur)=>{
+            return prev+cur;
+        })
+    },
+    /**
+     * 将数组的值转化为对象的val
+     * @param {*} arrName 
+     * @param {*} arrAge 
+     * @param {*} arrDec 
+     */
+    arrToObject(arrName,arrAge,arrDec){
+        return arrName.map((item,index)=>{
+            return { name: item, age: arrAge[index],dec:arrDec[index]}
+        })
+    }
+    /**
      * 分割指定长度的元素数组
      * @param {*} list 
      * @param {*} size 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let CommonArray = require('./commonArray');
-let arr= [1,3,4,33,5,6];
+const arr1 = [1, 2, 3, 4, 5, 8, 9]
+const arr2 = [5, 6, 7, 8, 9];
 // console.log(CommonArray.getArrCommon(arr,[1,3,4,5,6],[0,3]))
-console.log(CommonArray.spliceArrList(arr,2));
+console.log(CommonArray.getArrAll(arr1,arr2));
